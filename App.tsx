de393280e15bc093162776db1c75ac9734f62150
Vaishali Yadav <hr@HRs-Mac-mini.local>
2024-02-12 17:08:59 +0530

    installation

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+
+
+const App = () => {
+  return (<NavigationContainer>
+    
+  </NavigationContainer>);
+};
+
+export default App;
