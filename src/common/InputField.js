6ebdbbea5aef0bba3e5d57b558703f8e1f6b5269
Vaishali Yadav <hr@HRs-Mac-mini.local>
2024-02-14 11:15:05 +0530

    sign up page completed

--- src/common/InputField.js
+++ src/common/InputField.js
@@ -1,19 +1,34 @@
-import {View, StyleSheet, TextInput,Text} from 'react-native';
+import {View, StyleSheet, TextInput, Text} from 'react-native';
 import ResponsiveSize from '../utils/responsivesSize';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import {useState} from 'react';
 
-const InputField = ({field}) => {
+const InputField = ({field , state}) => {
+  const [isHidden, setIsHidden] = useState(false);
+
+  const toggleVisibility = () => {
+    setIsHidden(!isHidden);
+  };
+
   return (
     <View style={styles.namecon}>
-      <TextInput style={styles.name} placeholder={field} />
-      {/* <TouchableOpacity style={styles.hide}>
-        <Text>Show</Text>
-      </TouchableOpacity> */}
+      <TextInput
+        style={styles.name}
+        placeholder={field}
+        secureTextEntry={isHidden ? true : false}
+      />
+{state &&
+    <TouchableOpacity onPress={toggleVisibility}>
+      <Text style={styles.hidentext}>{isHidden ? 'Show' : 'Hide'}</Text>
+    </TouchableOpacity>}
     </View>
   );
 };
 const styles = StyleSheet.create({
   namecon: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
     backgroundColor: '#F6F6F6',
     borderWidth: 1,
     borderColor: '#E8E8E8',
@@ -22,12 +37,16 @@ const styles = StyleSheet.create({
     padding: ResponsiveSize(5),
   },
   name: {
-    paddingLeft: ResponsiveSize(12),
-    fontWeight: '400',
+    paddingLeft: ResponsiveSize(10),
+    fontWeight: '500',
     fontSize: 16,
-    // fontFamily:'Inter Regular Regular'
+    fontFamily: 'Inter Regular Regular',
   },
-  
+  hidentext: {
+    color: '#5DB075',
+    paddingRight: ResponsiveSize(10),
+    fontFamily: 'Inter Regular Regular'
+  },
 });
 
 export default InputField;
