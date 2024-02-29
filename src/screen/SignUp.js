ccd55ff45854d084fb7592cdd50d0484440f8f44
Vaishali Yadav <hr@HRs-Mac-mini.local>
2024-02-29 10:50:31 +0530

    signup completed

--- src/screen/SignUp.js
+++ src/screen/SignUp.js
@@ -2,28 +2,31 @@ import React from 'react';
 import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 import CheckBox from 'react-native-check-box';
 import ResponsiveSize from '../utils/responsivesSize';
-import {COLORS, IMAGES,FONTS} from '../utils/constants';
+import {COLORS, IMAGES, FONTS} from '../utils/constants';
 import Header from '../common/header';
 import InputField from '../common/InputField';
 import Button from '../common/Button';
 import {useState} from 'react';
 
-
-const SignUp = (props) => {
-  const [isSelected, setSelection] = useState(false);
+const SignUp = props => {
+  const [checked, setChecked] = useState(false);
 
   const navigateToLogin = () => {
-    props.navigation.navigate('Login'); 
+    props.navigation.navigate('Login');
   };
 
-  const toggle = () => {
-    setSelection(!isSelected);
+  const toggleCheckBox = () => {
+    setChecked(!checked);
   };
 
   return (
     <View style={styles.container}>
       <View>
-        <Header image={IMAGES.cross} onPress={navigateToLogin} textOne={'Sign Up'} />
+        <Header
+          image={IMAGES.cross}
+          onPress={navigateToLogin}
+          textOne={'Sign Up'}
+        />
       </View>
       <View style={styles.field}>
         <InputField field={'Name'} />
@@ -31,12 +34,12 @@ const SignUp = (props) => {
         <InputField field={'Password'} state />
       </View>
       <View style={styles.textCon}>
-        <TouchableOpacity onPress={toggle}>
-          <CheckBox
-            value={isSelected}
-            onValueChange={setSelection}
-            style={styles.checkBox}
-          />
+        <TouchableOpacity onPress={toggleCheckBox}>
+          <View style={styles.checkBoxContainer}>
+            <View style={styles.checkBox}>
+              {checked && <Text style={styles.checkIcon}>✓</Text>}
+            </View>
+          </View>
         </TouchableOpacity>
         <Text style={styles.unText}>
           I would like to receive your newsletter and other promotional
@@ -62,8 +65,20 @@ const styles = StyleSheet.create({
     margin: ResponsiveSize(9),
     marginTop: ResponsiveSize(25),
   },
+  checkBoxContainer: {
+    flexDirection: 'row',
+  },
   checkBox: {
-    marginRight: ResponsiveSize(6),
+    width: ResponsiveSize(16),
+    height: ResponsiveSize(16),
+    backgroundColor: COLORS.lGrey,
+    borderRadius: 4,
+    marginRight: ResponsiveSize(5),
+    marginTop: ResponsiveSize(3),
+  },
+  checkIcon: {
+    color: COLORS.green,
+    fontSize: 14,
   },
   unText: {
     fontFamily: FONTS.interRegular,
